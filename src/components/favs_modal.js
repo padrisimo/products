6aa499ff3511a47683beb1c782579659e4440618
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-09 13:19:17 +0100

    listing favorites

--- src/components/favs_modal.js
+++ src/components/favs_modal.js
@@ -5,20 +5,47 @@ import { showModal, hideModal } from '../actions'
 import Modal from 'react-bootstrap4-modal';
 
 class FavsModal extends Component {
-    onClose(){
+    onClose() {
         this.props.hideModal();
     }
-    modalBackdropClicked(){
+    modalBackdropClicked() {
         alert('go banana, go! go! banana!');
     }
     render() {
+        const { visible, items, favs } = this.props;
+        if (favs.length == 0) {
+            return (
+                <Modal visible={visible} onClickBackdrop={this.modalBackdropClicked}>
+                    <div className="modal-header">
+                        <h5 className="modal-title">Only Macarroni :(</h5>
+                    </div>
+                    <div className="modal-body">
+                        <h5>U may choose some favorites!</h5>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-danger" onClick={() => this.onClose()}>
+                            Close
+                        </button>
+                    </div>
+                </Modal>
+            );
+        }
         return (
-            <Modal visible={this.props.visible} onClickBackdrop={this.modalBackdropClicked}>
+            <Modal visible={visible} onClickBackdrop={this.modalBackdropClicked}>
                 <div className="modal-header">
                     <h5 className="modal-title">U love those so much!</h5>
                 </div>
                 <div className="modal-body">
-                    <p>Enemy vessel approaching!</p>
+                    <ul className="list-group">
+                        {items.filter(item => favs.includes(item.id)).map(
+                            fav => (
+                                <li className="list-group-item justify-content-between">
+                                    {fav.title}
+                                    <span className="badge badge-default badge-pill">14</span>
+                                </li>
+                            )
+                        )}
+                    </ul>
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-danger" onClick={() => this.onClose()}>
@@ -32,7 +59,9 @@ class FavsModal extends Component {
 
 const mapStateToProps = (state) => ({
     visible: state.modal.visible,
-    isfetched: state.modal.isfetched
+    isfetched: state.modal.isfetched,
+    items: state.catalog.items,
+    favs: state.love.favs,
 });
 
 export default connect(mapStateToProps, { showModal, hideModal })(FavsModal)
